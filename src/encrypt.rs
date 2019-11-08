086b97a28985bdd621b621f9f8f01adc9a4b0f9b
Jack Grigg <thestr4d@gmail.com>
2019-11-08 15:19:10 +0000

    Encryption interface

---
+++ src/encrypt.rs
@@ -0,0 +1,345 @@
+use age::{Encryptor, RecipientKey};
+use gtk::prelude::*;
+use gtk::{FileChooserAction, FileChooserDialog, Inhibit, Orientation::Vertical, ResponseType};
+use relm::{Channel, EventStream, Relm, Widget};
+use relm_derive::{widget, Msg};
+use std::fs::File;
+use std::io::{self, Write};
+use std::path::PathBuf;
+use std::thread;
+
+use self::EncryptMsg::*;
+
+const MINIMUM_PASSPHRASE_LENGTH: u16 = 6;
+
+pub struct Model {
+    filenames: Vec<PathBuf>,
+    recipients: Vec<(String, RecipientKey)>,
+    stream: EventStream<EncryptMsg>,
+    encrypt_channel: Option<Channel<io::Result<()>>>,
+}
+
+#[derive(Msg)]
+pub enum EncryptMsg {
+    Change,
+    StartEncrypt,
+    FinishEncrypt,
+    Close,
+}
+
+#[allow(clippy::cognitive_complexity)]
+#[widget]
+impl Widget for Win {
+    fn model(relm: &Relm<Self>, filenames: Vec<PathBuf>) -> Model {
+        assert!(!filenames.is_empty());
+        Model {
+            filenames,
+            recipients: vec![],
+            stream: relm.stream().clone(),
+            encrypt_channel: None,
+        }
+    }
+
+    fn init_view(&mut self) {
+        self.to_passphrase.join_group(Some(&self.to_recipients));
+
+        if self.model.filenames.len() == 1 {
+            // Encrypting a single file. The file list never changes, so we only need to
+            // set this up once.
+            self.encrypt_separately.set_visible(false);
+        }
+
+        self.update_ui();
+    }
+
+    fn update(&mut self, event: EncryptMsg) {
+        match event {
+            Change => self.update_ui(),
+            StartEncrypt => self.model.encrypt_channel = self.select_encrypt_output(),
+            FinishEncrypt => {
+                self.window.close();
+            }
+            Close => self.window.close(),
+        }
+    }
+
+    view! {
+        #[name="window"]
+        gtk::Window {
+            title: "Encrypt Files - Girage",
+            delete_event(_, _) => (Close, Inhibit(false)),
+
+            gtk::Box {
+                orientation: Vertical,
+
+                gtk::Frame {
+                    label: Some("Encrypt"),
+                    margin_top: 10,
+                    margin_bottom: 10,
+                    margin_start: 10,
+                    margin_end: 10,
+
+                    gtk::Box {
+                        orientation: Vertical,
+
+                        gtk::Box {
+                            #[name="to_recipients"]
+                            gtk::RadioButton {
+                                label: "Encrypt to recipients:",
+                                clicked => Change,
+                            },
+
+                            #[name="add_recipient"]
+                            gtk::Entry {
+                            },
+                        },
+
+                        #[name="recipients"]
+                        gtk::TreeView {
+                        },
+
+                        gtk::Box {
+                            #[name="to_passphrase"]
+                            gtk::RadioButton {
+                                label: "Encrypt with passphrase:",
+                                clicked => Change,
+                            },
+
+                            #[name="passphrase"]
+                            gtk::Entry {
+                                visibility: false,
+                                changed => Change,
+                            },
+                        },
+                    },
+                },
+
+                gtk::Frame {
+                    label: Some("Output"),
+                    margin_bottom: 10,
+                    margin_start: 10,
+                    margin_end: 10,
+
+                    gtk::Box {
+                        orientation: Vertical,
+
+                        #[name="armor"]
+                        gtk::CheckButton {
+                            label: "Generate ASCII-armored output.",
+                        },
+
+                        #[name="encrypt_separately"]
+                        gtk::CheckButton {
+                            label: "Encrypt each file separately.",
+                        },
+                    },
+                },
+
+                gtk::ActionBar {
+                    #[name="encrypt"]
+                    gtk::Button {
+                        label: "Encrypt",
+                        clicked => StartEncrypt,
+                    },
+
+                    gtk::Button {
+                        label: "Cancel",
+                        clicked => Close,
+                    },
+                }
+            },
+        }
+    }
+}
+
+impl Win {
+    fn update_ui(&mut self) {
+        let to_recipients = self.to_recipients.get_active();
+        self.add_recipient.set_sensitive(to_recipients);
+        self.passphrase.set_sensitive(!to_recipients);
+
+        self.encrypt.set_sensitive(if to_recipients {
+            !self.model.recipients.is_empty()
+        } else {
+            self.passphrase.get_text_length() >= MINIMUM_PASSPHRASE_LENGTH
+        });
+    }
+
+    fn select_encrypt_output(&self) -> Option<Channel<io::Result<()>>> {
+        let input_paths = self.model.filenames.clone();
+        let multiple_files = input_paths.len() > 1;
+        let encrypt_separately = self.encrypt_separately.get_active();
+        let armor = self.armor.get_active();
+
+        let dialog = if multiple_files {
+            if encrypt_separately {
+                FileChooserDialog::new(
+                    Some("Select a folder"),
+                    Some(&self.window),
+                    FileChooserAction::CreateFolder,
+                )
+            } else {
+                let dialog = FileChooserDialog::new(
+                    Some("Save encrypted archive"),
+                    Some(&self.window),
+                    FileChooserAction::Save,
+                );
+                dialog.set_do_overwrite_confirmation(true);
+                dialog.set_filename(
+                    input_paths[0]
+                        .parent()
+                        .expect("has parent")
+                        .join("archive.tar.age"),
+                );
+                dialog
+            }
+        } else {
+            let dialog = FileChooserDialog::new(
+                Some("Save encrypted file"),
+                Some(&self.window),
+                FileChooserAction::Save,
+            );
+            dialog.set_do_overwrite_confirmation(true);
+            dialog.set_filename(age_extension(&self.model.filenames[0]));
+            dialog
+        };
+        dialog.add_button("Cancel", ResponseType::Cancel);
+        dialog.add_button("Save", ResponseType::Accept);
+        let result = dialog.run();
+        let output_path = dialog.get_filename();
+        dialog.destroy();
+
+        if result == ResponseType::Accept {
+            let encryptor = if self.to_recipients.get_active() {
+                Encryptor::Keys(
+                    self.model
+                        .recipients
+                        .iter()
+                        .map(|(_, k)| k)
+                        .cloned()
+                        .collect(),
+                )
+            } else {
+                Encryptor::Passphrase(
+                    self.passphrase
+                        .get_text()
+                        .expect("get_text failed")
+                        .to_string(),
+                )
+            };
+
+            let spinner = gtk::Spinner::new();
+            let dialog = gtk::Dialog::new_with_buttons(
+                Some("Encrypting..."),
+                Some(&self.window),
+                gtk::DialogFlags::MODAL,
+                &[],
+            );
+            dialog
+                .get_content_area()
+                .pack_start(&spinner, true, true, 20);
+
+            // TODO: Figure out why spinner doesn't show up.
+            spinner.start();
+            spinner.show();
+            dialog.show();
+
+            let stream = self.model.stream.clone();
+            let (channel, sender) = Channel::new(move |res| {
+                if let Err(e) = res {
+                    // TODO: Do something useful with the error.
+                    eprintln!("{}", e);
+                }
+                dialog.destroy();
+                stream.emit(FinishEncrypt);
+            });
+
+            thread::spawn(move || {
+                sender
+                    .send(encrypt(
+                        encryptor,
+                        input_paths,
+                        output_path.expect("have filename"),
+                        armor,
+                        encrypt_separately,
+                    ))
+                    .expect("send message");
+            });
+
+            Some(channel)
+        } else {
+            None
+        }
+    }
+}
+
+fn age_extension(filename: &PathBuf) -> PathBuf {
+    let mut output_filename = filename.clone();
+    if let Some(ext) = filename.extension() {
+        output_filename.set_extension(format!(
+            "{}.age",
+            ext.to_str().expect("extension is valid UTF-8")
+        ));
+    } else {
+        output_filename.set_extension("age");
+    };
+    output_filename
+}
+
+fn encrypt(
+    encryptor: Encryptor,
+    input_paths: Vec<PathBuf>,
+    output_path: PathBuf,
+    armor: bool,
+    encrypt_separately: bool,
+) -> io::Result<()> {
+    if input_paths.len() > 1 {
+        if encrypt_separately {
+            for input_path in &input_paths {
+                encrypt_single_file(
+                    &encryptor,
+                    input_path,
+                    age_extension(
+                        &output_path.join(input_path.file_name().expect("valid filename")),
+                    ),
+                    armor,
+                )?;
+            }
+            Ok(())
+        } else {
+            encrypt_archive(&encryptor, input_paths, output_path, armor)
+        }
+    } else {
+        encrypt_single_file(&encryptor, &input_paths[0], output_path, armor)
+    }
+}
+
+fn encrypt_single_file(
+    encryptor: &Encryptor,
+    input_path: &PathBuf,
+    output_path: PathBuf,
+    armor: bool,
+) -> io::Result<()> {
+    let mut input = File::open(input_path)?;
+    let mut output = encryptor.wrap_output(File::create(output_path)?, armor)?;
+    io::copy(&mut input, &mut output)?;
+    output.flush()
+}
+
+fn encrypt_archive(
+    encryptor: &Encryptor,
+    input_paths: Vec<PathBuf>,
+    output_path: PathBuf,
+    armor: bool,
+) -> io::Result<()> {
+    let mut archive = tar::Builder::new(encryptor.wrap_output(File::create(output_path)?, armor)?);
+    for input_path in &input_paths {
+        let mut input = File::open(input_path)?;
+        archive.append_file(
+            input_path.file_name().expect("filename is valid UTF-8"),
+            &mut input,
+        )?;
+    }
+    let mut output = archive.into_inner()?;
+    output.flush()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+use gtk::prelude::*;
+use gtk::{FileChooserAction, FileChooserDialog, FileChooserExt, Inhibit, ResponseType};
+use relm::{init, Component, Widget};
+use relm_derive::{widget, Msg};
+
+use self::Msg::*;
+
+mod encrypt;
+
+pub struct Model {
+    encrypt: Option<Component<encrypt::Win>>,
+}
+
+#[derive(Msg)]
+pub enum Msg {
+    OpenEncrypt,
+    Quit,
+}
+
+#[widget]
+impl Widget for Win {
+    fn model() -> Model {
+        Model { encrypt: None }
+    }
+
+    fn update(&mut self, event: Msg) {
+        match event {
+            OpenEncrypt => self.open_encrypt(),
+            Quit => gtk::main_quit(),
+        }
+    }
+
+    view! {
+        #[name="window"]
+        gtk::Window {
+            title: "Girage",
+            delete_event(_, _) => (Quit, Inhibit(false)),
+
+            gtk::HeaderBar {
+                gtk::Button {
+                    label: "Encrypt",
+                    clicked => OpenEncrypt,
+                },
+            },
+        }
+    }
+}
+
+impl Win {
+    fn open_encrypt(&mut self) {
+        let dialog = FileChooserDialog::new(
+            Some("Open a file"),
+            Some(&self.window),
+            FileChooserAction::Open,
+        );
+        dialog.set_select_multiple(true);
+        dialog.add_button("Cancel", ResponseType::Cancel);
+        dialog.add_button("Open", ResponseType::Accept);
+        let result = dialog.run();
+        if result == ResponseType::Accept {
+            self.model.encrypt =
+                Some(init::<encrypt::Win>(dialog.get_filenames()).expect("encrypt window"));
+        }
+        dialog.destroy();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Win::run(()).unwrap();
 }
